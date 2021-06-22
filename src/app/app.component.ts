3b2951dd0f2c433aaade6c4da5b66f33ef7f13e1
Prajwal Makanwar <prajwal.makanwar@gmail.com>
2021-06-22 14:50:04 +0530

    Initial Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title: string = 'Counter App';
+  
+  count: number = 0;
+
+  handleIncrease = () =>{
+    if(this.count < 10)
+    {
+      this.count = this.count + 1;
+    }
+  }
+
+
+  handleDecrease = () =>{
+    this.count = this.count -1;
+    if(this.count < 0){
+      this.count = 0;
+    }
+  }
+
+  handleReset = () =>{
+    this.count = 0;
+
+  }
+
 }
